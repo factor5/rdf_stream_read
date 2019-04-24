cca7ac57336d8982cc788f3809646919a5a23d71
Svilen Velikov <svilen.velikov@sirma.bg>
2019-04-24 17:34:21 +0300

    remove unused imports and logging

--- client.js
+++ client.js
@@ -1,23 +1,12 @@
 const axios = require('axios');
 const httpAdapter = require('axios/lib/adapters/http');
-const fs = require('fs');
 const N3 = require('n3');
 const streamParser = new N3.StreamParser();
 
-const OUTPUT = 'output.ttl';
-
-if (fs.existsSync(OUTPUT)) {
-    fs.unlinkSync(OUTPUT);
-}
-
-const output = fs.createWriteStream(OUTPUT);
-
-//request the file
 axios.get('http://localhost:3000/', {
     responseType: 'stream', 
     adapter: httpAdapter
 }).then((response) => {
-    console.log('got response');
     const rdfStream = response.data;
     rdfStream.pipe(streamParser);
     streamParser.pipe(new SlowConsumer());
